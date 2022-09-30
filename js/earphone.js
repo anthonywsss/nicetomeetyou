26291f1faffb33d643985e31d38dc5f681020917
anthonywsss <wijayasaputranthony@gmail.com>
2022-09-30 14:33:04 +0700

    Initial Commit

---
+++ js/earphone.js
@@ -0,0 +1,8 @@
+swal({
+    title: "Halo Sobat Atisa !",
+    text: "Pake earphone yaa dengerinnya",
+    icon: "warning",
+    button: "Siap Boss !",
+  });
+
+  
---
+++ js/phoneorientation.js
@@ -0,0 +1,8 @@
+if(window.innerHeight > window.innerWidth){
+    swal({
+        title: "Halo Sobat Atisa !",
+        text: "Pake Laptop/Landscape View Yaaa Biar Enak Liatnya",
+        icon: "success",
+        button: "Siap Boss !",
+      });
+}
